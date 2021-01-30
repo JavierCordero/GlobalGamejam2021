42823ee328a027dae28eb41a42712d0a4f0dc719
Daniel Martin <dani.martin.blazquez@gmail.com>
2021-01-30 16:42:23 +0100

    Añadido luz al player

--- Assets/Scripts/LightModifier.cs
+++ Assets/Scripts/LightModifier.cs
@@ -7,8 +7,12 @@ public class LightModifier : MonoBehaviour
     Light[] lightsOnScene;
     float[] intensityInitialValues;
 
+    public Light lightP1 = null, lightP2 = null;
+
     [Range(0f, 1f)]
     public float lightOffIntensity = 0.1f;
+    [Range(0f, 1f)]
+    public float playerLightOnIntensity = 1f;
     [Range(0f, 1f)]
     public float turnOnOffSpeed = 0.1f;
 
@@ -26,38 +30,86 @@ public class LightModifier : MonoBehaviour
         }
     }
 
-    public void TurnOnLights()
+    public void TurnOnLights(bool isPlayer1)
     {
         if (turnOnOffWithLerp)
         {
             for (int i = 0; i < lightsOnScene.Length; ++i)
             {
-                StartCoroutine(LerpLightIntensity(i, intensityInitialValues[i], turnOnOffSpeed, errorTolerance));
+                if (lightsOnScene[i]==lightP1|| lightsOnScene[i]==lightP2)
+                {
+                    if (isPlayer1&& lightsOnScene[i]==lightP1)
+                    {
+                        StartCoroutine(LerpLightIntensity(i, intensityInitialValues[i], turnOnOffSpeed, errorTolerance));
+                    }
+                    else if(!isPlayer1 && lightsOnScene[i]==lightP2)
+                    {
+                        StartCoroutine(LerpLightIntensity(i, intensityInitialValues[i], turnOnOffSpeed, errorTolerance));
+                    }
+                }
+                else
+                    StartCoroutine(LerpLightIntensity(i, intensityInitialValues[i], turnOnOffSpeed, errorTolerance));
             }
         }
         else
         {
-            for (int i = 0; i < intensityInitialValues.Length; ++i)
+            for (int i = 0; i < lightsOnScene.Length; ++i)
             {
-                lightsOnScene[i].intensity = intensityInitialValues[i];
+                if (lightsOnScene[i] == lightP1 || lightsOnScene[i]== lightP2)
+                {
+                    if (isPlayer1 && lightsOnScene[i] == lightP1)
+                    {
+                        lightsOnScene[i].intensity = intensityInitialValues[i];
+                    }
+                    else if (!isPlayer1 && lightsOnScene[i] == lightP2)
+                    {
+                        lightsOnScene[i].intensity = intensityInitialValues[i];
+                    }
+                }
+                else
+                    lightsOnScene[i].intensity = intensityInitialValues[i];
             }
         }
     }
 
-    public void TurnOffLights()
+    public void TurnOffLights(bool isPlayer1)
     {
         if (turnOnOffWithLerp)
         {
             for(int i=0; i<lightsOnScene.Length; ++i)
             {
-                StartCoroutine(LerpLightIntensity(i, lightOffIntensity, turnOnOffSpeed, errorTolerance));
+                if (lightsOnScene[i] == lightP1 || lightsOnScene[i] == lightP2)
+                {
+                    if (isPlayer1 && lightsOnScene[i] == lightP1)
+                    {
+                        StartCoroutine(LerpLightIntensity(i, playerLightOnIntensity, turnOnOffSpeed, errorTolerance));
+                    }
+                    else if (!isPlayer1 && lightsOnScene[i] == lightP2)
+                    {
+                        StartCoroutine(LerpLightIntensity(i, playerLightOnIntensity, turnOnOffSpeed, errorTolerance));
+                    }
+                }
+                else
+                    StartCoroutine(LerpLightIntensity(i, lightOffIntensity, turnOnOffSpeed, errorTolerance));
             }
         }
         else
         {
             for (int i = 0; i < intensityInitialValues.Length; ++i)
             {
-                lightsOnScene[i].intensity = lightOffIntensity;
+                if (lightsOnScene[i] == lightP1 || lightsOnScene[i] == lightP2)
+                {
+                    if (isPlayer1 && lightsOnScene[i] == lightP1)
+                    {
+                        lightsOnScene[i].intensity = playerLightOnIntensity;
+                    }
+                    else if (!isPlayer1 && lightsOnScene[i] == lightP2)
+                    {
+                        lightsOnScene[i].intensity = playerLightOnIntensity;
+                    }
+                }
+                else
+                    lightsOnScene[i].intensity = lightOffIntensity;
             }
         }
     }
