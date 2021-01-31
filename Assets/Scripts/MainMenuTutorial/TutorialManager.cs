63b3dcabb79640a02b7d58efa2af482df8f82c61
Daniel Quintero <daquinte@ucm.es>
2021-01-31 14:51:33 +0100

    menu guay

--- Assets/Scripts/MainMenuTutorial/TutorialManager.cs
+++ Assets/Scripts/MainMenuTutorial/TutorialManager.cs
@@ -9,6 +9,15 @@ public class TutorialManager : MonoBehaviour
     Text [] texts;
     int index = 0;
 
+    [Header("Audio Settings")]
+    public AudioSource menuMusic;
+
+    public void Start()
+    {
+        menuMusic.volume = 0.5f;
+        menuMusic.Play();
+    }
+
     public void ChangeText()
     {
         texts[index].gameObject.SetActive(false);
